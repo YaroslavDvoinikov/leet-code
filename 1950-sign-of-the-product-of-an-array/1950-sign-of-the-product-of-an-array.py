d4edce7750a5668cefc756e86ebccef492d5b3b7
Yaroslav Dvoinikov <jaroslavs.dvoinikovs@gmail.com>
2025-09-28 15:20:21 +0300

    Time: 0 ms (100%), Space: 12.6 MB (54.41%) - LeetHub

--- 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.py
+++ 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.py
@@ -1,11 +1,15 @@
-class Solution:
-    def arraySign(self, nums: List[int]) -> int:
-        num = 1
+class Solution(object):
+    def arraySign(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        neg_count = 0
         for i in range(len(nums)):
-            num = num*nums[i]
-        print(num)
-        if num>0:
+            if nums[i] == 0:
+                return 0
+            if nums[i] < 0:
+                neg_count += 1
+        if neg_count%2 == 0:
             return 1
-        elif num<0:
-            return -1
-        return 0
+        return -1
