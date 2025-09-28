f37ead1a88cddef91e5a219a186c03fa6eaf9b9d
Yaroslav Dvoinikov <jaroslavs.dvoinikovs@gmail.com>
2025-09-28 18:17:36 +0300

    Time: 0 ms (100%), Space: 12.4 MB (75.21%) - LeetHub

--- 1626-can-make-arithmetic-progression-from-sequence/1626-can-make-arithmetic-progression-from-sequence.py
+++ 1626-can-make-arithmetic-progression-from-sequence/1626-can-make-arithmetic-progression-from-sequence.py
@@ -6,7 +6,7 @@ class Solution(object):
         """
         arr.sort()
         diff = arr[1] - arr[0]
-        for i in range(len(arr)-1):
+        for i in range(1,len(arr)-1):
             if arr[i+1] - arr[i] != diff:
                 return False
         return True
